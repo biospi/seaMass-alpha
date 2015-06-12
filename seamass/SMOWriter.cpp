d46c95ed7285743bfc27e5b08560e7852173ad18
Andrew Dowsey <andrew@dowsey.net>
2015-06-12 18:45:50 +0100

    went back to opening existing smo file as needed to write multiple preset scan configs

--- seamass/SMOWriter.cpp
+++ seamass/SMOWriter.cpp
@@ -28,13 +28,17 @@
 SMOWriter::
 SMOWriter(const string& filename)
 {
-    file = H5Fcreate(filename.c_str(), H5F_ACC_TRUNC, H5P_DEFAULT, H5P_DEFAULT);
-    if (file < 0)
-    {
-            // throw exception
-            cerr << "problem opening smo" << endl;
-            throw "problem opening smo";
-    }
+	file = H5Fcreate(filename.c_str(), H5F_ACC_EXCL, H5P_DEFAULT, H5P_DEFAULT);
+	if (file < 0)
+	{
+		file = H5Fopen(filename.c_str(), H5F_ACC_RDWR, H5P_DEFAULT);
+		if (file < 0)
+		{
+			// throw exception
+			cerr << "problem opening smo" << endl;
+			throw "problem opening smo";
+		}
+	}
 }
 
 
@@ -46,7 +50,8 @@ SMOWriter::
     {
         // throw exception
         cerr << "problem closing smo" << endl;
-    }
+		throw "problem closing smo";
+	}
 }
 
 
