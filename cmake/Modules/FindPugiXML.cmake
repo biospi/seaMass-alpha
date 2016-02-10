d6f5b86cd3591d3fbacaff0472cb95de50736295
Andrew Dowsey <andrew.dowsey@manchester.ac.uk>
2016-02-10 18:46:45 +0000

    had to add back FindPugiXML and FindnetCDF so they would be found on Linux

---
+++ cmake/Modules/FindPugiXML.cmake
@@ -0,0 +1,30 @@
+# Find the pugixml XML parsing library.
+#
+# Sets the usual variables expected for find_package scripts:
+#
+# PUGIXML_INCLUDE_DIR - header location
+# PUGIXML_LIBRARIES - library to link against
+# PUGIXML_FOUND - true if pugixml was found.
+
+find_path (PugiXML_INCLUDE_DIR
+           NAMES pugixml.hpp
+           PATHS ${PugiXML_DIR}/include)
+find_library (PugiXML_LIBRARY
+              NAMES pugixml
+              PATHS ${PugiXML_DIR}/lib)
+
+# Support the REQUIRED and QUIET arguments, and set PUGIXML_FOUND if found.
+include (FindPackageHandleStandardArgs)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS (PugiXML FOUND_VAR PugiXML_FOUND REQUIRED_VARS PugiXML_INCLUDE_DIR PugiXML_LIBRARY)
+
+if (PugiXML_FOUND)
+    set (PugiXML_LIBRARIES ${PugiXML_LIBRARY})
+    if (NOT PugiXML_FIND_QUIETLY)
+        message (STATUS "PugiXML include = ${PugiXML_INCLUDE_DIR}")
+        message (STATUS "PugiXML library = ${PugiXML_LIBRARY}")
+    endif ()
+else (PugiXML_FOUND)
+    message (STATUS "No PugiXML found")
+endif(PugiXML_FOUND)
+
+mark_as_advanced (PugiXML_LIBRARY PugiXML_INCLUDE_DIR)
--- cmake_icc.sh
+++ cmake_icc.sh
@@ -4,14 +4,14 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export CPLUS_INCLUDE_PATH=$CPLUS_INCLUDE_PATH:/usr/include/x86_64-linux-gnu/c++/4.*
  
 mkdir $DIR/build
-mkdir $DIR/build/gcc
+mkdir $DIR/build/icc
 
-mkdir $DIR/build/gcc/debug
-pushd $DIR/build/gcc/debug
+mkdir $DIR/build/icc/debug
+pushd $DIR/build/icc/debug
 cmake -DCMAKE_C_COMPILER=icc -DCMAKE_CXX_COMPILER=icc -DCMAKE_BUILD_TYPE=Debug $@ ../../..
 popd
 
-mkdir $DIR/build/gcc/release
-pushd $DIR/build/gcc/release
+mkdir $DIR/build/icc/release
+pushd $DIR/build/icc/release
 cmake -DCMAKE_C_COMPILER=icc -DCMAKE_CXX_COMPILER=icc -DCMAKE_BUILD_TYPE=Release $@ ../../..
 popd
